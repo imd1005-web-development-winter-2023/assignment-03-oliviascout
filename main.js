4451abd0f56f9b5cbffbcf46052c32f56597c632
oliviascout <oliviascoutmalek@gmail.com>
2023-03-18 10:54:32 -0400

    the code is not working for some reason? im comparing it to previous stuff to see if theres any big difference

--- main.js
+++ main.js
@@ -9,19 +9,25 @@
 
 // Constants
 const appID = "app";
-const headingText = "To do. To done. ✅";
+//const headingText = "To do. To done. ✅";
 
 // Variables
 
 // DOM Elements
 let appContainer = document.getElementById(appID);
 
+//array of items and the related things
+const items = [];
+const itemList = document.querySelector("item-list");
+const itemForm = document.querySelector("add-item");
+const itemName = document.querySelector("item-name");
+
 //
 // Functions
 //
 
-// Add a heading to the app container
-function inititialise() {
+// Add a heading to the app container\
+/*function inititialise() {
   // If anything is wrong with the app container then end
   if (!appContainer) {
     console.error("Error: Could not find app contianer");
@@ -35,10 +41,53 @@ function inititialise() {
 
   // Init complete
   console.log("App successfully initialised");
+}*/
+
+
+//for when the form is submitted
+function addItem(){
+  //stop defulat submission
+  e.preventDefault();
+
+  //get text
+  const itemName = item.value();
+  items.push(itemName);
+
+  //add and draw new list
+  updateList(items, itemList);
+
+  //reset form
+  itemForm.reset();
+}
+
+//actually adding the item
+function updateList(stuff, stuffList){
+  //clean the list
+  while(stuffList.firstChild){
+    stuffList.removeChild(stuffList.firstChild);
+  }
+
+  //add list
+  for(let i = 0; i < stuff.length; i++){
+    const listStuff = document.createElement("li");
+    listStuff.textContent = stuff[i];
+
+    //animation thing
+    if(i == stuff.length - 1){
+      listStuff,classList,add("new-item-annimate");
+    }
+
+    //add item to list
+    stuffList.appendChild(listStuff);
+
+  }
 }
 
 //
 // Inits & Event Listeners
 //
+
+listForm.addEventListener("submit", addItem);
+updateList(items, itemList);
 
 inititialise();
